41e40642141fa73d076d14b141565b42f2311dc4
Priyanka Sengupta <78321696+Priyanka-01-s@users.noreply.github.com>
2023-11-30 00:17:23 +0530

    Counting the frequency of words in a paragraph

--- src/Main.java
+++ src/Main.java
@@ -5,30 +5,40 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
         System.out.println("HASHMAP TO CALCULATE WORD OCCURANCE\n");
-        String text = "To be or not to be";
+        //String text = "To be or not to be";
         
-    
-        String[] words = TextFormatting.cleanTexts(text);
+        String paragraph = "Paranoids are not paranoid because they are paranoid but "
+                + "because they keep putting themselves deliberately into paranoid avoidable situations";
+        
+        //clean and processed text
+        String[] words = TextFormatting.cleanTexts(paragraph);
 
+        // Hash table to store word frequencies
         Map<Integer, LinkedList<Node>> hashMap = new HashMap<>();
 
+         // Process each word in the array
         for(String word: words){
             int hash = HashWordCount.getHash(word);
+            // Check if the hash is already a key
             if(!hashMap.containsKey(hash)){
                 hashMap.put(hash, new LinkedList<>());
             }
 
             LinkedList<Node> linkedList = hashMap.get(hash);
 
+            // Check if the word is already in the list
             Node existingNode = HashWordCount.CountNodeByKey(linkedList, word);
 
             if(existingNode != null){
+                //increment the value if found
                 existingNode.value++;
             }else{
+                //if not then add the new node to the linkedlist
                 linkedList.add(new Node(word,1));
             }
         }
 
+        //Displaying the words and the frequency
         for (Map.Entry<Integer, LinkedList<Node>> entry : hashMap.entrySet()) {
             LinkedList<Node> linkedList = entry.getValue();
             for (Node node : linkedList) {
